5384a7d15544db41cca07b186c10c0b5ef3eda37
David <awarri74@gmail.com>
2025-02-25 00:43:51 +0100

    ready for deployment1

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, Blueprint, Response
+from flask import Flask, request, jsonify, Blueprint
 from flask_cors import CORS
 from flask_compress import Compress
 from flask_limiter import Limiter
